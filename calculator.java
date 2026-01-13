7a5bbdd82f695b87164cd41094aabe9b021b54ab
utkarsh <utkarshraj309@gmail.com>
2026-01-13 14:55:33 +0530

    [utkarsh]modified

--- calculator.java
+++ calculator.java
@@ -2,8 +2,10 @@ public class calculator{
     public static void main(String[] args){
         System.out.println("Hello world");
     }
-}
 
-public static void multiply(int a,int b){
+    public static void multiply(int a,int b){
     System.out.println(a*b);
 }
+}
+
+

81c78e963b9a5f09ca114ed6c9e66573f7606bdc
Aditya Singh <adityasingh4082002@gmail.com>
2026-01-13 14:57:52 +0530

    A

--- calculator.java
+++ calculator.java
@@ -13,7 +13,7 @@ public class calculator{
         System.out.println("Hello world");
     }
     public static void multiply(int a,int b){
-    System.out.println(a/b);
+    System.out.println(c/b);
 }
 }
 

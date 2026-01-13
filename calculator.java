ce5d347a5c868c663547d842aac3faed8edde9c8
Aditya Singh <adityasingh4082002@gmail.com>
2026-01-13 14:37:40 +0530

    [Aditya] subtraction method

--- calculator.java
+++ calculator.java
@@ -1,4 +1,9 @@
 public class calculator{
+
+    public static void subtraction(int a,int b){
+        System.out.println("subtraction : " + (a-b));
+    }
+
     public static void main(String[] args){
         System.out.println("Hello world");
     }

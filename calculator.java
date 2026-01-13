71dbb3a376a3072e23a6b725b6e9be2d41f90e39
Aditya Singh <adityasingh4082002@gmail.com>
2026-01-13 14:52:55 +0530

    A

--- calculator.java
+++ calculator.java
@@ -1,23 +1,20 @@
 public class calculator{
-<<<<<<< HEAD
 
     public static void subtraction(int a,int b){
         System.out.println("subtraction : " + (a-b));
     }
-
-=======
     public void add(int a,int b){
         int result=a+b;
         System.out.println("addition:"+result);
     }
->>>>>>> fdee929e07e32a2703048d22863ab812d13d9120
     public static void main(String[] args){
         
         
         System.out.println("Hello world");
     }
+    public static void multiply(int a,int b){
+    System.out.println(a/b);
+}
 }
 
-public static void multiply(int a,int b){
-    System.out.println(a*b);
-}
+
